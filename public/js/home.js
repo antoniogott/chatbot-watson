e6d1e2abdbc717542d36c698e47f2e9db85f8d22
Antonio Gottsfritz <antoniobgott@gmail.com>
2019-07-12 04:43:04 -0300

    UI & socket.io events

---
+++ public/js/home.js
@@ -0,0 +1,57 @@
+var socket = io.connect();
+
+$('form#chat').submit(function (e) {
+    e.preventDefault();
+
+    socket.emit('send msg', $(this).find('#msgBox').val(), function () {
+        $('form#chat #msgBox').val('');
+    });
+});
+
+socket.on('own msg', function (msg) {
+    const content = $('<p />')
+        .addClass('text-left')
+        .addClass('mx-3')
+        .addClass('my-1')
+        .css('font-size', '1.1rem')
+        .html(msg);
+
+    const innerDiv = $('<div />')
+        .addClass('shadow-sm')
+        .addClass('rounded')
+        .addClass('bg-white')
+        .addClass('w-auto')
+        .append(content);
+
+    const outerDiv = $('<div />')
+        .addClass('d-flex')
+        .addClass('flex-row-reverse')
+        .addClass('my-3')
+        .append(innerDiv);
+
+    $('#messages').append(outerDiv);
+});
+
+socket.on('chatbot msg', function (msg) {
+    const content = $('<p />')
+        .addClass('text-left')
+        .addClass('mx-3')
+        .addClass('my-1')
+        .css('font-size', '1.1rem')
+        .html(msg);
+
+    const innerDiv = $('<div />')
+        .addClass('shadow-sm')
+        .addClass('rounded')
+        .addClass('bg-primary')
+        .addClass('text-white')
+        .addClass('w-auto')
+        .append(content);
+
+    const outerDiv = $('<div />')
+        .addClass('d-flex')
+        .addClass('my-3')
+        .append(innerDiv);
+
+    $('#messages').append(outerDiv);
+});
